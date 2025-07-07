fc097cf2abf3734d2500832ba58caa42cac95ca2
TanMinh-MCN <fctanminh0304@gmail.com>
2025-07-08 01:21:12 +0700

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -28,17 +28,17 @@ function App() {
   const [status, setStatus] = useState<Status>(Status.LETS_PLAY);
   const [pointTarget, setPointTarget] = useState(1);
   const [isFinished, setIsFinished] = useState(false);
-  // const [timeInPoint, setTimeInPoint] = useState<number>(200);
   const gameOverRef = useRef<boolean>(false);
+  const versionRef = useRef<number>(0);
+  const isAutoPlayRef = useRef<boolean>(false);
 
   useEffect(() => {
-    if (status === Status.GAME_OVER) {
+    if (status === Status.GAME_OVER) 
       gameOverRef.current = true;
-    }
   }, [status]);
 
   useEffect(() => {
-    let interval: any;
+    let interval: any, timeout: any;
     if (isRunning && !isFinished) {
       interval = setInterval(() => {
         setTimer(timer => timer + 1);
@@ -46,27 +46,32 @@ function App() {
     }
 
     if (pointTarget - 1 === Number(amount)) {
-      setTimeout(() => {
+      timeout = setTimeout(() => {
         setStatus(Status.ALL_CLEARED);
         setIsRunning(false);
         setIsFinished(true);
       }, 2000);
     }
 
-    return () => clearInterval(interval)
+    return () => {
+      clearInterval(interval);
+      clearTimeout(timeout);
+    }
   }, [isRunning, pointTarget]);
 
-  const handleReset = () => {
+
+  const handleStart = () => {
+    // Clear all state
+    versionRef.current = versionRef.current + 1;
     setTimer(0);
-    setPointTarget(1);
     setPoints([]);
+    setPointTarget(1);
     setStatus(Status.LETS_PLAY);
     setIsRunning(false);
     setIsFinished(false);
-  }
-
-  const handleStart = () => {
     setIsRunning(true);
+    isAutoPlayRef.current = false;
+    gameOverRef.current = false;
 
     for (let i = Number(amount); i >= 1; i--) {
       const x = Math.floor(Math.random() * 650) + 1;
@@ -77,9 +82,11 @@ function App() {
 
   const delay = (seconds: number) => new Promise(resolve => setTimeout(resolve, seconds * 1000));
 
-  const handleAutoPlay = async () => {
+  const handleAutoPlay = async (version: number) => {
     setIsRunning(true);
-    for (let i = 1; i <= Number(amount); i++) {
+    isAutoPlayRef.current = !isAutoPlayRef.current;
+
+    for (let i = pointTarget; i <= Number(amount) && isAutoPlayRef.current; i++) {
       setPointTarget(pointTarget => pointTarget + 1);
 
       setPoints(points =>
@@ -91,7 +98,6 @@ function App() {
       for (let j = 0; j < 20; j++) {
         if (gameOverRef.current) return;
         await delay(0.1);
-        // setTimeInPoint(timeInPoint => timeInPoint - 10);
         setPoints(points =>
           points.map(point =>
             point.number === i ? { ...point, timer: point.timer - 10, opacity: point.opacity - 5 } : point
@@ -99,30 +105,13 @@ function App() {
         );
       }
 
-      // setTimeInPoint(200);
-      setPoints(points => points.filter(point => point.number !== i));
-      // setPoints(points =>
-      //   points.map(point =>
-      //     point.number === i ? { ...point, clicked: true } : point
-      //   )
-      // );
-
-      // for (let i = 0; i < 20; i++) {
-      //   await delay(0.1);
-      //   setTimeInPoint(timeInPoint => timeInPoint - 10);
-      // }
-
-      // setPointTarget(pointTarget => pointTarget + 1);
-
-      // setTimeInPoint(200);
-      // setPoints(points => points.filter(point => point.number !== i));
-
-      // await delay(1);
+      if(version === versionRef.current) {
+        setPoints(points => points.filter(point => point.number !== i));
+      }
     }
-    setIsRunning(false);
   }
 
-  const handleClick = async (number: number) => {
+  const handleClick = async (number: number, version: number) => {
     if (number === pointTarget && isRunning) {
       setPointTarget(pointTarget => pointTarget + 1);
 
@@ -135,7 +124,6 @@ function App() {
       for (let i = 0; i < 20; i++) {
         if (gameOverRef.current) return;
         await delay(0.1);
-        // setTimeInPoint(timeInPoint => timeInPoint - 10);
         setPoints(points =>
           points.map(point =>
             point.number === number ? { ...point, timer: point.timer - 10, opacity: point.opacity - 5 } : point
@@ -143,8 +131,9 @@ function App() {
         );
       }
 
-      // setTimeInPoint(200);
-      setPoints(points => points.filter(point => point.number !== number));
+      if(version === versionRef.current) {
+        setPoints(points => points.filter(point => point.number !== number));
+      }
 
     } else {
       setStatus(Status.GAME_OVER);
@@ -160,7 +149,8 @@ function App() {
         <div className='flex flex-col justify-between'>
           <p className='text-xl'>Points: </p>
           <p className='text-xl'>Time: </p>
-          {/* <p className='text-xl'>{(timeInPoint / 100).toFixed(2)}</p> */}
+          <p className='text-xl'>{pointTarget}</p>
+          <p className='text-xl'>{points.map(point => point.number).join(', ')}</p>
         </div>
         <div className='flex flex-col justify-between'>
           <input value={amount} onChange={(e) => {
@@ -173,14 +163,14 @@ function App() {
         {!isRunning && !isFinished ? <button disabled={isRunning} onClick={handleStart} className='bg-blue-500 text-white px-10 py-1 rounded-md hover:bg-blue-600 hover:cursor-pointer'>Start</button>
           :
           <>
-            <button className='bg-blue-500 text-white px-10 py-1 rounded-md hover:bg-blue-600 hover:cursor-pointer' onClick={handleReset}>Restart</button>
-            <button disabled={!isRunning} onClick={handleAutoPlay} className='bg-blue-500 text-white px-10 py-1 rounded-md hover:bg-blue-600 hover:cursor-pointer'>Auto Play</button>
+            <button className='bg-blue-500 text-white px-10 py-1 rounded-md hover:bg-blue-600 hover:cursor-pointer' onClick={handleStart}>Restart</button>
+            {!isFinished && <button disabled={!isRunning} onClick={() => handleAutoPlay(versionRef.current)} className='bg-blue-500 text-white px-10 py-1 rounded-md hover:bg-blue-600 hover:cursor-pointer'>Auto Play {!isAutoPlayRef.current ? 'ON' : 'OFF'}</button>}
           </>}
       </div>
       <div className='w-1/2 h-115 border-2 border-gray-300 rounded-md mt-3 relative'>
         {points.map(point => (
           <div
-            onClick={() => handleClick(point.number)}
+            onClick={() => handleClick(point.number, versionRef.current)}
             key={point.number}
             className={`
               absolute w-13 h-13 rounded-full border border-red-500 flex items-center justify-center
